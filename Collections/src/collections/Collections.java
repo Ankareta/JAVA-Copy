f043c196bcb4d81a7683f184e96cc850721b3cad
ankareta <carolinankaret@gmail.com>
2023-10-06 11:58:17 -0600

    se agregan las array list

--- Collections/src/collections/Collections.java
+++ Collections/src/collections/Collections.java
@@ -13,13 +13,27 @@ public class Collections {
 		System.out.println(listaDeCompras);
 		
 		
-		//Imprimir info de listaDeCompras con un forEach
+		//Imprimir info de listaDeCompras con un foSite 105ssssssssrEach
 		//Para el dato de tipo String que llamaremos productoAImprimir de la lista de compras, lo vamos a tomar y lo vamos a imprimir
 				for(String productoAImprimir : listaDeCompras) {
 					
 					System.out.println(productoAImprimir);
 				}//foreach
-		
+				//Implementacion de un arrayList
+				//Clase Genera (interfaz), tipo de dato que usaré, nombre de mí estructura de datos, instancia de dicha clase general
+				List<String> listaDeContactos = new ArrayList<> ();
+				
+				//Agrego elementos de tipo String a mi arrayList llamado listaDeContactos
+				listaDeContactos.add("Felipe");
+				listaDeContactos.add("Roberto");
+				listaDeContactos.add("Alicia");
+				listaDeContactos.add("Fatima");
+				
+				//Obtener un elemento de a lista - asignarlo a una variable
+				String datoFelipe = listaDeContactos.get(0);
+				
+				System.out.println(datoFelipe);
+				
 	}//metodo main
 	
 	
@@ -36,5 +50,15 @@ Es una estructura de datos lineal que contiene elementos del mismo tipo. Los ele
 -No proporciona metodos adicionales para agregar o eliminar elementos
 
 Un ejemplo de array es una lista de compras del supermercado
+
+-ArrayList
+ * 
+ * Es una implementacion de una lista (List), es decir, que esta en un nivel abajo de las listas. Utiliza un array dinamico que permite almadenar datos. 
+ * 
+ * Sus principales características son:
+ * 
+ * 	-	Tamaño dinámico, a diferencia de los arrays tradicionales, el tamaño de un aarayist si se puede modificar. 
+ *  -	Acceso rápido, podemos acceder a un elemento del arrayist por su posición oindice
+ *  -	Coste elevado al momento de insertar o eliminar elementos. Si agrego un elemento, tengo que recorrer todos los demas elementos. 
 */
 }//collections
